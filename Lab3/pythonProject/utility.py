060b0b28f242adac332ce3186839d8daf94885c1
xGhosty <134649417+KrystianLabak@users.noreply.github.com>
2024-11-27 10:06:55 +0100

    Add files via upload

---
+++ Lab3/pythonProject/utility.py
@@ -0,0 +1,14 @@
+def inputValue(msg, start = 0, end = None):
+    while True:
+        inp = input(msg)
+
+        if not inp.isdecimal():
+            print("Nieprawidłowe dane wejściowe")
+        elif start is not None and end is not None:
+            if not (start <= int(inp) <= end):
+                print("Podano błędne dane")
+            else:
+                return int(inp)
+
+        else:
+            return int(inp)
