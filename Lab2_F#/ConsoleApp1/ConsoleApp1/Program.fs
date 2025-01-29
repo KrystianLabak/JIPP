ebf6c3b4c3d6fedd1c3d30b5d83c18ff9d034a29
xGhosty <134649417+KrystianLabak@users.noreply.github.com>
2025-01-29 08:55:22 +0100

    Add files via upload

---
+++ Lab2_F#/ConsoleApp1/ConsoleApp1/Program.fs
@@ -0,0 +1,24 @@
+﻿open System.IO
+open System
+
+//Napisz program, który pobiera tekst od użytkownika, a następnie oblicza i wyświetla:
+//• Liczbę słów w podanym tekście.
+//• Liczbę znaków (bez spacji).
+
+
+//liczenie slow
+let countWords(text: string) = 
+    text.Split([|' '; '\t';'\n';';';':';'.'|], StringSplitOptions.RemoveEmptyEntries)
+    |>Array.length
+
+//liczenie znakow bez spacji
+let countChars (text: string) =
+    text.Replace(" ","").Length
+
+printf "Podaj teskt: "
+let inputText = Console.ReadLine()
+
+let wordsCount = countWords inputText
+let charCount = countChars inputText
+
+printfn "Liczba słów: %d, a liczba znaków(bez spacji): %d" wordsCount charCount
